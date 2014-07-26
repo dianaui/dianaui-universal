gwtbootstrap3/gwtbootstrap3

Fixed Issue #133 - Extended DeviceSize to have the different sizes and fixed the HasResponsiveness interface to use DeviceSize to make it easier.

gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/Modal.java
@@ -25,16 +25,7 @@ import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.web.bindery.event.shared.HandlerRegistration;
-import org.gwtbootstrap3.client.shared.event.ModalHiddenEvent;
-import org.gwtbootstrap3.client.shared.event.ModalHiddenHandler;
-import org.gwtbootstrap3.client.shared.event.ModalHideEvent;
-import org.gwtbootstrap3.client.shared.event.ModalHideHandler;
-import org.gwtbootstrap3.client.shared.event.ModalShowEvent;
-import org.gwtbootstrap3.client.shared.event.ModalShowHandler;
-import org.gwtbootstrap3.client.shared.event.ModalShownEvent;
-import org.gwtbootstrap3.client.shared.event.ModalShownHandler;
-import org.gwtbootstrap3.client.ui.base.HasResponsiveness;
-import org.gwtbootstrap3.client.ui.base.helper.StyleHelper;
+import org.gwtbootstrap3.client.shared.event.*;
 import org.gwtbootstrap3.client.ui.base.modal.ModalContent;
 import org.gwtbootstrap3.client.ui.base.modal.ModalDialog;
 import org.gwtbootstrap3.client.ui.constants.Attributes;
@@ -87,7 +78,7 @@ import org.gwtbootstrap3.client.ui.html.Div;
  * @see org.gwtbootstrap3.client.shared.event.ModalHideEvent
  * @see org.gwtbootstrap3.client.shared.event.ModalHiddenEvent
  */
-public class Modal extends Div implements IsClosable, HasResponsiveness {
+public class Modal extends Div implements IsClosable {
     private final String TOGGLE = "toggle";
     private final String HIDE = "hide";
     private final String SHOW = "show";
@@ -170,16 +161,6 @@ public class Modal extends Div implements IsClosable, HasResponsiveness {
                 }
             });
         }
-    }
-
-    @Override
-    public void setVisibleOn(final String deviceSizeString) {
-        StyleHelper.setVisibleOn(this, deviceSizeString);
-    }
-
-    @Override
-    public void setHiddenOn(final String deviceSizeString) {
-        StyleHelper.setHiddenOn(this, deviceSizeString);
     }
 
     /**

dianaui/dianaui-universal

Conflicts:
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/ButtonToolBar.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/Divider.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/DropDownHeader.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/IconStack.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/Image.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/InputGroup.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/ListItem.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/Modal.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/ModalBody.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/ModalFooter.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/ModalHeader.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/NavbarCollapse.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/NavbarCollapseButton.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/NavbarHeader.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/NavbarText.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/base/AbstractButtonGroup.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/base/AbstractListItem.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/base/AbstractTextWidget.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/base/ComplexWidget.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/base/ValueBoxBase.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/base/button/Caret.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/base/form/FormElementContainer.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/gwt/CellTable.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/gwt/DataGrid.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/gwt/FlowPanel.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/gwt/HTMLPan

gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/Modal.java
@@ -68,7 +68,7 @@ import org.gwtbootstrap3.client.ui.constants.HasResponsiveness;
  * @see ModalBody
  * @see ModalFooter
  */
-public class Modal extends ModalWithBackdrop implements IsClosable, HasResponsiveness {
+public class Modal extends ModalWithBackdrop implements IsClosable {
 
     private final ModalContent content = new ModalContent();
     private final ModalDialog dialog = new ModalDialog();
@@ -108,16 +108,6 @@ public class Modal extends ModalWithBackdrop implements IsClosable, HasResponsiv
                 }
             });
         }
-    }
-
-    @Override
-    public void setVisibleOn(final String deviceSizeString) {
-        StyleHelper.setVisibleOn(this, deviceSizeString);
-    }
-
-    @Override
-    public void setHiddenOn(final String deviceSizeString) {
-        StyleHelper.setHiddenOn(this, deviceSizeString);
     }
 
     @Override
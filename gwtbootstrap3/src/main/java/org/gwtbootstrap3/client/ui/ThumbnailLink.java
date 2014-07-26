gwtbootstrap3/gwtbootstrap3

Adding a way to set the target of the Anchor/ThumbnailLink with HasTarget interface and the AttributeMixin.

gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/ThumbnailLink.java
@@ -26,12 +26,11 @@ import com.google.gwt.event.dom.client.*;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.Focusable;
-import org.gwtbootstrap3.client.ui.base.ComplexWidget;
-import org.gwtbootstrap3.client.ui.base.HasDataToggle;
-import org.gwtbootstrap3.client.ui.base.HasHref;
-import org.gwtbootstrap3.client.ui.base.HasTargetHistoryToken;
+import org.gwtbootstrap3.client.ui.base.*;
+import org.gwtbootstrap3.client.ui.base.mixin.AttributeMixin;
 import org.gwtbootstrap3.client.ui.base.mixin.DataToggleMixin;
 import org.gwtbootstrap3.client.ui.base.mixin.FocusableMixin;
+import org.gwtbootstrap3.client.ui.constants.Attributes;
 import org.gwtbootstrap3.client.ui.constants.Toggle;
 
 /**
@@ -52,9 +51,10 @@ import org.gwtbootstrap3.client.ui.constants.Toggle;
  * @author Joshua Godi
  */
 public class ThumbnailLink extends ComplexWidget implements HasClickHandlers, HasDoubleClickHandlers, HasHref,
-        HasDataToggle, HasTargetHistoryToken, Focusable {
+        HasDataToggle, HasTargetHistoryToken, Focusable, HasTarget {
 
     private final DataToggleMixin<ThumbnailLink> toggleMixin = new DataToggleMixin<ThumbnailLink>(this);
+    private final AttributeMixin<ThumbnailLink> attributeMixin = new AttributeMixin<ThumbnailLink>(this);
     private final FocusableMixin<ThumbnailLink> focusableMixin;
     private String targetHistoryToken;
 
@@ -172,5 +172,21 @@ public class ThumbnailLink extends ComplexWidget implements HasClickHandlers, Ha
     @Override
     public void setFocus(final boolean focused) {
         focusableMixin.setFocus(focused);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setTarget(final String target) {
+        attributeMixin.setAttribute(Attributes.TARGET, target);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getTarget() {
+        return attributeMixin.getAttribute(Attributes.TARGET);
     }
 }

dianaui/dianaui-universal

Conflicts:
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/Anchor.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/ThumbnailLink.java

gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/ThumbnailLink.java
@@ -29,17 +29,14 @@ import com.google.gwt.user.client.ui.Focusable;
 import org.gwtbootstrap3.client.ui.base.ComplexWidget;
 import org.gwtbootstrap3.client.ui.base.mixin.FocusableMixin;
 import org.gwtbootstrap3.client.ui.base.mixin.ToggleMixin;
-import org.gwtbootstrap3.client.ui.constants.HasHref;
-import org.gwtbootstrap3.client.ui.constants.HasTargetHistoryToken;
-import org.gwtbootstrap3.client.ui.constants.HasToggle;
-import org.gwtbootstrap3.client.ui.constants.Toggle;
+import org.gwtbootstrap3.client.ui.constants.*;
 
 /**
  * @author Joshua Godi
  * @author <a href='mailto:donbeave@gmail.com'>Alexey Zhokhov</a>
  */
 public class ThumbnailLink extends ComplexWidget implements HasClickHandlers, HasDoubleClickHandlers, HasHref,
-        HasToggle, HasTargetHistoryToken, Focusable {
+        HasToggle, HasTargetHistoryToken, Focusable, HasTarget {
 
     private final ToggleMixin<ThumbnailLink> toggleMixin = new ToggleMixin<ThumbnailLink>(this);
     private FocusableMixin<ThumbnailLink> focusableMixin;
@@ -125,6 +122,26 @@ public class ThumbnailLink extends ComplexWidget implements HasClickHandlers, Ha
             focusableMixin = new FocusableMixin<ThumbnailLink>(this);
         }
         return focusableMixin;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getTarget() {
+        return getAnchorElement().getTarget();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void setTarget(String target) {
+        getAnchorElement().setTarget(target);
+    }
+
+    private AnchorElement getAnchorElement() {
+        return AnchorElement.as(getElement());
     }
 
 }
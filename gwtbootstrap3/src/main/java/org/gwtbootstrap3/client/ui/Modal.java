b4824a3368a055a5e621a5394142f5dd746903c2
BeaVe <donbeave@gmail.com>
2014-08-01 15:09:09 +0800

    from code review
    Conflicts:
    	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/Modal.java

--- gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/Modal.java
+++ gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/Modal.java
@@ -23,17 +23,11 @@ package org.gwtbootstrap3.client.ui;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.web.bindery.event.shared.HandlerRegistration;
-import org.gwtbootstrap3.client.shared.event.*;
 import org.gwtbootstrap3.client.ui.base.helper.StyleHelper;
 import org.gwtbootstrap3.client.ui.base.modal.ModalContent;
 import org.gwtbootstrap3.client.ui.base.modal.ModalDialog;
 import org.gwtbootstrap3.client.ui.base.modal.ModalWithBackdrop;
 import org.gwtbootstrap3.client.ui.constants.Attributes;
-import org.gwtbootstrap3.client.ui.constants.IconType;
-import org.gwtbootstrap3.client.ui.constants.ModalBackdrop;
-import org.gwtbootstrap3.client.ui.constants.Styles;
-import org.gwtbootstrap3.client.ui.html.Div;
 
 /**
  * Modal dialog.
@@ -90,7 +84,7 @@ public class Modal extends ModalWithBackdrop implements IsClosable {
     public void setKeyboard(final boolean keyboard) {
         getElement().setAttribute(Attributes.DATA_KEYBOARD, Boolean.toString(keyboard));
     }
-    
+
     public void setSize(ModalSize size) {
         StyleHelper.addUniqueEnumStyleName(dialog, ModalSize.class, size);
     }

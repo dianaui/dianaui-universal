gwtbootstrap3/gwtbootstrap3

Added setIconFixedWidth() and isIconFixedWidth() to HasIcon.
- Added option to set fixed width to Icon which is part of another widget.
  Other Icon properties were already part of HasIcon interface.
  Issue is covered in #208.

gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/base/mixin/IconTextMixin.java
@@ -50,6 +50,7 @@ public class IconTextMixin<T extends ComplexWidget & HasText & HasIcon & HasIcon
     private boolean iconSpin = false;
     private boolean iconBordered = false;
     private boolean iconLight = false;
+    private boolean iconFixedWidth = false;
 
     public IconTextMixin(final T widget) {
         this.widget = widget;
@@ -168,6 +169,17 @@ public class IconTextMixin<T extends ComplexWidget & HasText & HasIcon & HasIcon
         return iconSpin;
     }
 
+    @Override
+    public void setIconFixedWidth(final boolean iconFixedWidth) {
+        this.iconFixedWidth = iconFixedWidth;
+        render();
+    }
+
+    @Override
+    public boolean isIconFixedWidth() {
+        return iconFixedWidth;
+    }
+
     private void render() {
         // We defer to make sure the elements are available to manipulate their positions
         Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
@@ -193,6 +205,7 @@ public class IconTextMixin<T extends ComplexWidget & HasText & HasIcon & HasIcon
                 icon.setSpin(iconSpin);
                 icon.setBorder(iconBordered);
                 icon.setLight(iconLight);
+                icon.setFixedWidth(iconFixedWidth);
 
                 // Since we are dealing with Icon/Text, we can insert them at the right position
                 // Helps on widgets like ButtonDropDown, where it has a caret added

dianaui/dianaui-universal

Conflicts:
	core/src/main/java/com/dianaui/universal/core/client/ui/Anchor.java
	core/src/main/java/com/dianaui/universal/core/client/ui/InputGroupAddon.java
	core/src/main/java/com/dianaui/universal/core/client/ui/base/AbstractAnchorListItem.java
	core/src/main/java/com/dianaui/universal/core/client/ui/base/AbstractFormElement.java
	core/src/main/java/com/dianaui/universal/core/client/ui/base/HasIcon.java
	core/src/main/java/com/dianaui/universal/core/client/ui/base/button/AbstractIconButton.java
	gwtbootstrap3/src/main/java/org/gwtbootstrap3/client/ui/base/mixin/IconTextMixin.java

core/src/main/java/com/dianaui/universal/core/client/ui/base/mixin/IconTextMixin.java
@@ -55,6 +55,7 @@ public class IconTextMixin<T extends ComplexWidget & HasText & HasHTML & HasIcon
     private boolean iconSpin = false;
     private boolean iconBordered = false;
     private boolean iconLight = false;
+    private boolean iconFixedWidth = false;
 
     public IconTextMixin(final T widget) {
         this.widget = widget;
@@ -251,6 +252,19 @@ public class IconTextMixin<T extends ComplexWidget & HasText & HasHTML & HasIcon
         }
     }
 
+    @Override
+    public void setIconFixedWidth(final boolean iconFixedWidth) {
+        this.iconFixedWidth = iconFixedWidth;
+        if (fontAwesomeIcon != null && fontAwesomeIcon.isSpin() != iconSpin) {
+            render();
+        }
+    }
+
+    @Override
+    public boolean isIconFixedWidth() {
+        return iconFixedWidth;
+    }
+
     private void render() {
         // We defer to make sure the elements are available to manipulate their positions
         Scheduler.get().scheduleDeferred(new Scheduler.ScheduledCommand() {
@@ -276,6 +290,7 @@ public class IconTextMixin<T extends ComplexWidget & HasText & HasHTML & HasIcon
                     fontAwesomeIcon.setSpin(iconSpin);
                     fontAwesomeIcon.setBorder(iconBordered);
                     fontAwesomeIcon.setLight(iconLight);
+                    fontAwesomeIcon.setFixedWidth(iconFixedWidth);
                 }
 
                 // Since we are dealing with Icon/Text, we can insert them at the right position
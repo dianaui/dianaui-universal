b4b55f210a9c1cfc4271aa4684891574f167c12d
Alexey <donbeave@gmail.com>
2014-10-10 21:44:09 +0800

    gwtbootstrap3 files moved to dianaui package.

--- core/src/main/java/com/dianaui/universal/core/client/ui/FocusWidget.java
+++ core/src/main/java/com/dianaui/universal/core/client/ui/FocusWidget.java
@@ -1,14 +1,33 @@
-package org.gwtbootstrap3.client.ui;
+/*
+ * #%L
+ * Diana UI Core
+ * %%
+ * Copyright (C) 2014 Diana UI
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.dianaui.universal.core.client.ui;
 
-import org.gwtbootstrap3.client.ui.base.HasId;
-import org.gwtbootstrap3.client.ui.base.HasPull;
-import org.gwtbootstrap3.client.ui.base.HasResponsiveness;
-import org.gwtbootstrap3.client.ui.base.helper.StyleHelper;
-import org.gwtbootstrap3.client.ui.base.mixin.IdMixin;
-import org.gwtbootstrap3.client.ui.base.mixin.PullMixin;
-import org.gwtbootstrap3.client.ui.constants.DeviceSize;
-import org.gwtbootstrap3.client.ui.constants.Pull;
-import org.gwtbootstrap3.client.ui.constants.Styles;
+import com.dianaui.universal.core.client.ui.base.HasId;
+import com.dianaui.universal.core.client.ui.base.HasPull;
+import com.dianaui.universal.core.client.ui.base.HasResponsiveness;
+import com.dianaui.universal.core.client.ui.base.helper.StyleHelper;
+import com.dianaui.universal.core.client.ui.base.mixin.IdMixin;
+import com.dianaui.universal.core.client.ui.base.mixin.PullMixin;
+import com.dianaui.universal.core.client.ui.constants.DeviceSize;
+import com.dianaui.universal.core.client.ui.constants.Pull;
+import com.dianaui.universal.core.client.ui.constants.Styles;
 import com.google.gwt.dom.client.Element;
 
 public abstract class FocusWidget extends com.google.gwt.user.client.ui.FocusWidget implements HasResponsiveness,

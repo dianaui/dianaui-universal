a7454a3ae71a33ae749f322083ec147b2e922927
Alexey <donbeave@gmail.com>
2014-10-15 16:10:05 +0800

    Improved ButtonGroup tests.

--- core/src/test/java/com/dianaui/universal/core/client/ui/ButtonGroupTest.java
+++ core/src/test/java/com/dianaui/universal/core/client/ui/ButtonGroupTest.java
@@ -1,5 +1,3 @@
-package com.dianaui.universal.core.client.ui;
-
 /*
  * #%L
  * Diana UI Core
@@ -19,8 +17,13 @@ package com.dianaui.universal.core.client.ui;
  * limitations under the License.
  * #L%
  */
+package com.dianaui.universal.core.client.ui;
 
+import com.dianaui.universal.core.client.ui.constants.ButtonGroupSize;
 import com.dianaui.universal.core.client.ui.constants.Styles;
+import com.dianaui.universal.core.client.ui.constants.Toggle;
+import com.google.gwt.dom.client.Document;
+import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.googlecode.gwt.test.GwtModule;
 import com.googlecode.gwt.test.GwtTest;
@@ -35,6 +38,46 @@ import static com.googlecode.gwt.test.assertions.GwtAssertions.assertThat;
  */
 @GwtModule("com.dianaui.universal.core.DianaUICore")
 public class ButtonGroupTest extends GwtTest {
+
+    // TODO toggle attr helper
+    String DATA_TOGGLE_BUTTONS = "data-toggle=\"" + Toggle.BUTTONS.getToggle() + "\"";
+
+    static String getHtml(String content, String styles, String attributes) {
+        return "<div class=\"" + Styles.BTN_GROUP + (styles != null ? " " + styles : "") + "\"" +
+                (attributes != null ? " " + attributes : "") + ">" + (content != null ? content : "") + "</div>";
+    }
+
+    @Test
+    public void defaults() {
+        ButtonGroup group = new ButtonGroup();
+        assertThat(group).hasStyle(Styles.BTN_GROUP);
+        Assert.assertFalse(group.isOpen());
+        Assert.assertFalse(group.isJustified());
+        Assert.assertEquals(getHtml(null, null, null), group.getElement().toString());
+
+        group.setJustified(true);
+        Assert.assertEquals(getHtml(null, Styles.BTN_GROUP_JUSTIFIED, null), group.getElement().toString());
+
+        group.setJustified(false);
+        group.setToggle(Toggle.BUTTONS);
+        Assert.assertEquals(getHtml(null, null, DATA_TOGGLE_BUTTONS), group.getElement().toString());
+
+        group.setToggle(null);
+        group.setSize(ButtonGroupSize.EXTRA_SMALL);
+        Assert.assertEquals(getHtml(null, ButtonGroupSize.EXTRA_SMALL.getCssName(), null), group.getElement().toString());
+
+        group.setSize(ButtonGroupSize.DEFAULT);
+        group.setDropUp(true);
+        Assert.assertEquals(getHtml(null, Styles.DROP_UP, null), group.getElement().toString());
+
+        group.setDropUp(false);
+        group.show();
+        Assert.assertEquals(getHtml(null, Styles.OPEN, null), group.getElement().toString());
+        Assert.assertTrue(group.isOpen());
+
+        group.hide();
+        Assert.assertEquals(getHtml(null, null, null), group.getElement().toString());
+    }
 
     @Test
     public void clickOnCheckBox() {
@@ -95,6 +138,15 @@ public class ButtonGroupTest extends GwtTest {
             Assert.assertTrue(button2.getValue());
             assertThat(button1).doesNotHaveStyle(Styles.ACTIVE);
             assertThat(button2).hasStyle(Styles.ACTIVE);
+
+            // emulate click event on radio button2
+            button1.onBrowserEvent((Event) Document.get().createClickEvent(0, 0, 0, 0, 0, false, false, false, false));
+            button2.setValue(false, false);
+
+            Assert.assertTrue(button1.getValue());
+            Assert.assertFalse(button2.getValue());
+            assertThat(button1).hasStyle(Styles.ACTIVE);
+            assertThat(button2).doesNotHaveStyle(Styles.ACTIVE);
         } finally {
             group.removeFromParent();
         }

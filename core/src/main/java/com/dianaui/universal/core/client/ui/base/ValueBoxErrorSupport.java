4a72b35c072739d4119a7d6226ea00308daeda76
Alexey Zhokhov <donbeave@gmail.com>
2015-02-07 13:55:45 +0800

    ValueBoxErrorSupport's package changed.

--- core/src/main/java/com/dianaui/universal/core/client/ui/base/ValueBoxErrorSupport.java
+++ core/src/main/java/com/dianaui/universal/core/client/ui/base/ValueBoxErrorSupport.java
@@ -1,17 +1,15 @@
-package org.gwtbootstrap3.client.ui.base;
-
 /*
  * #%L
- * GwtBootstrap3
+ * Diana UI Core
  * %%
- * Copyright (C) 2013 GwtBootstrap3
+ * Copyright (C) 2014 Diana UI
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,16 +17,16 @@ package org.gwtbootstrap3.client.ui.base;
  * limitations under the License.
  * #L%
  */
+package com.dianaui.universal.core.client.ui.base;
 
-import java.util.List;
-
-import org.gwtbootstrap3.client.ui.HelpBlock;
-import org.gwtbootstrap3.client.ui.constants.ValidationState;
-
+import com.dianaui.universal.core.client.ui.HelpBlock;
+import com.dianaui.universal.core.client.ui.constants.ValidationState;
 import com.google.gwt.editor.client.EditorError;
 import com.google.gwt.event.logical.shared.AttachEvent;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.Widget;
+
+import java.util.List;
 
 public class ValueBoxErrorSupport implements ValueBoxBase.EditorErrorSupport {
 
